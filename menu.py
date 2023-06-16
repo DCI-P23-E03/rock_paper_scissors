7b0323e2ad8e48efd12e6264cb2afb7520a120a1
Letham01 <kusanagi29@ymail.com>
2023-06-16 13:08:43 +0200

    Added game logic and play again promt

--- menu.py
+++ menu.py
@@ -13,7 +13,31 @@ while flag1 == True:
 
     if menu == "1":
         print("Game starts")
-        break
+
+        while True:
+            choices = ["rock", "paper", "scissors"]
+            player_choice = input("Choose your move (rock, paper, scissors): ").lower()
+            computer_choice = random.choice(choices)
+
+            print("Player chooses:", player_choice)
+            print("Computer chooses:", computer_choice)
+
+            if player_choice == computer_choice:
+                print("It's a tie!")
+            elif (
+                (player_choice == "rock" and computer_choice == "scissors")
+                or (player_choice == "paper" and computer_choice == "rock")
+                or (player_choice == "scissors" and computer_choice == "paper")
+            ):
+                print("Player wins!")
+            else:
+                print("Computer wins!")
+
+            play_again = input("Do you want to play again? (yes/no): ")
+            if play_again.lower() != "yes":
+                break
+
+        continue
 
     elif menu == "2":
         flag2 = True
@@ -29,29 +53,37 @@ while flag1 == True:
 
             if highscore == "1":
                 print("List of players sorted with most wins")
-                list1 = input("Return to main menu with (m) or return to high score lists by pressing any key")
+                list1 = input(
+                    "Return to main menu with (m) or return to high score lists by pressing any key"
+                )
                 if list1 == "m":
                     flag2 = False
 
             elif highscore == "2":
                 print("List of players sorted with most games")
-                list2 = input("Return to main menu with (m) or return to high score lists by pressing any key")
+                list2 = input(
+                    "Return to main menu with (m) or return to high score lists by pressing any key"
+                )
                 if list2 == "m":
                     flag2 = False
 
             elif highscore == "3":
                 print("List of players sorted with most draws")
-                list3 = input("Return to main menu with (m) or return to high score lists by pressing any key")
+                list3 = input(
+                    "Return to main menu with (m) or return to high score lists by pressing any key"
+                )
                 if list3 == "m":
                     flag2 = False
 
             elif highscore == "4":
                 print("List of players sorted with most loses")
-                list4 = input("Return to main menu with (m) or return to high score lists by pressing any key")
+                list4 = input(
+                    "Return to main menu with (m) or return to high score lists by pressing any key"
+                )
                 if list4 == "m":
                     flag2 = False
-                    
-            else: 
+
+            else:
                 flag2 = False
 
     elif menu == "3":
@@ -61,4 +93,3 @@ while flag1 == True:
     else:
         print("Wrong input please try again")
         flag1 = True
-
