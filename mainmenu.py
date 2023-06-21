e99f9b740de7cfdd51f9dce581a4d3b5857a3994
DeeptiMalhotr <deeptimalhotrade@gmail.com>
2023-06-21 15:20:14 +0200

    code scoreboard changed2

--- mainmenu.py
+++ mainmenu.py
@@ -1,6 +1,7 @@
 from os import system as x
 import random
 
+# assigning the black value to the variables 
 player_name = ""
 player_age = ""
 total_wins = 0
@@ -12,6 +13,8 @@ choice1 = ""
 count = 0
 player_data ={}
 
+
+# the program will goes on on till the wanted to exit.
 while True:
     print('''
     [0] - Final Exit
@@ -22,14 +25,14 @@ while True:
 
 
     option = input("Select an option: ")
-
-    if option == "1":
-        total_wins = 0
+    
+    if option == "1": # Begining the play by taking the option "1"
+        total_wins = 0  # value "0" so that with every new player, variables are initialized or set as default.
         total_loses = 0
         total_draw = 0
             
         print("\nYuhuuu!!! Your game begins now......:")
-        player_name = input("\nEnter Your Name: ")
+        player_name = input("\nEnter Your Name: ") 
         #while (player_name.isalpha() != True):  
         #    print("Please enter the name using valid string..")
             
@@ -50,11 +53,12 @@ while True:
                 "Rock",
                 "Paper",
                 "Scissors",
-            ]
-            computer_choice1 = random.choice(choice1)
+            ] # option for random choice.
+            computer_choice1 = random.choice(choice1) # random module choice method is called so that every time computer shows a random option.
             player_choice =  input("Enter your choice: ").capitalize()
             print("Computer Choice: ",computer_choice1)
             
+            # comparing the player_choice and computer_choice so as show the win, lose, draw.
             if ((player_choice == "Rock" and computer_choice1 == "Scissors") or (player_choice == "Scissors" and computer_choice1 =="Paper") or (player_choice == "Paper" and computer_choice1 =="Rock")):
                 print("\nYipppiee..... You WON!!!!")
                 total_wins +=1
@@ -68,6 +72,7 @@ while True:
                 print("\nError........................Warning...................")        
 
 
+            # to check from user if he/she want to continue the game.
             yes_no = input("\nDo you wish to continue: y/n  : ").lower()
             if yes_no == "n":
                 print("\nOh!!! No Problem you play later......")
@@ -83,7 +88,7 @@ while True:
         continue
         
         
-    elif option == "2":
+    elif option == "2": # shows the highest scores of the play by taking the option "2" 
         print("\nThe Highest scores of Player......\n")
         
         player_list = [player_name, player_profile]
@@ -103,8 +108,8 @@ while True:
             break
 
 
-    elif option == "3":
-        print("\nHere you goes....The Scoreboard of the same:.........: ")
+    elif option == "3": # shows the scoreboard of the play by taking the option "3"
+        print("\nHere you goes....The Scoreboard of the Game:.........: \n")
 
         count = total_wins+total_loses+total_draw      
         '''player_name_profile = {"Name":player_name, "Player_Profile":player_profile}    
@@ -121,31 +126,27 @@ while True:
             "Total_Loses":total_loses,
             "Total_Draw":total_draw})
         '''
-        player_profile = {
-                        "Total Game":count,
-                        "Total Win":total_wins,
+        player_profile = { # sub dictionary of profile_data dictionary
+                        "Total Games":count,
+                        "Total Wins":total_wins,
                         "Total Loses":total_loses,
-                        "Total Draw":total_draw
+                        "Total Draws":total_draw
                         }
         
         #player_name = {"Player Name":player_name}
-        player_data.update({player_name:player_profile})
+        player_data.update({player_name:player_profile}) # updating the values in nested dictionary.
         #print("\nPlayer details on Scoreboard:   \n", player_data)
-
-        for playername, playerprofile in player_data.items():
+        
+        #for playername, playerprofile in player_data.items():
             #print("\nPlayer Name : ", playername)
-            for playerprofile, value in player_data.items(): 
-                print(f"{playerprofile} : {value}")
-            
-                
-
-
-      
-      
-        
-        
-
-    else:
+        for playerprofile, value in player_data.items(): 
+                print(f'''{playerprofile}
+Total Games: {value['Total Games']}''')
+                print(f"Total Wins: {value['Total Wins']}")
+                print(f"Total Loses: {value['Total Loses']}")
+                print(f"Total Draws: {value['Total Draws']}")
+    
+    else: # if wish to completly close/exit the game.
         print("Exiting.......")    
         break
     
