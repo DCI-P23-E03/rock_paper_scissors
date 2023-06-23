c3a13ddf75ca9f3746766310870dd8a29b53e876
DeeptiMalhotr <deeptimalhotrade@gmail.com>
2023-06-23 14:07:57 +0200

    new version of main menu3

--- mainmenuver3.py
+++ mainmenuver3.py
@@ -65,7 +65,8 @@ player_score = ""
 choice1 = ""
 total_games = 0
 player_data ={}
-
+most_wins = 0
+top_players = []
 
 # the program will goes on on till the wanted to exit.
 while True:
@@ -149,26 +150,27 @@ while True:
                         
         player_data.update({player_name:player_profile})    # updating the dictionary with player's name and it's profile
         
-        most_wins = 0
-        top_players = []
+        
 
-        for player_name1, player_profile1 in player_data.items():    # for loop for getting the player name and its profile details stored in player data dictonary
+        for player_name, player_profile in player_data.items():    # for loop for getting the player name and its profile details stored in player data dictonary
             total_wins = player_profile.get("Total Win",0)    # getting the value from player_profile dictionary for Total wins.
             
-            if total_wins > most_wins:      # comparing total_wins with most_wins
+            if total_wins >= most_wins:      # comparing total_wins with most_wins
                 most_wins = total_wins      # most_wins will be the highest total wins
-                top_players = [player_name1]   # top_players will be the player_name[s] who scored max total wins. Before adding [] each letter was considered player_name in this code
+                top_players.append(player_name)  # top_players will be the player_name[s] who scored max total wins. Before adding [] each letter was considered player_name in this code
 
             elif total_wins == most_wins:   # comparing if total_wins are tied with most_wins meaning a tie between multiple players
-                top_players.append(player_name1) # appending players tied with most_wins to top_players list
+                top_players.append(player_name) # appending players tied with most_wins to top_players list
 
-        if len(top_players) > 1:  # Checking if top_players list is longer than one player
+        if len(top_players) > 1 :  # Checking if top_players list is longer than one player
             print(f"\nWe have mutiple winners/Ties with {most_wins} wins :")
             for player in top_players:
                print('\033[92m'+player+'\033[0m')
+        
         elif most_wins == 0:   # check if the length of the top_player list is blank / zero or most_wins =0
             for player in top_players:  
                 print(f"\n\033[92m{player}\033[0m has not won any games yet...") 
+       
         else:       # If top_players list length is 1, message for one sole winner is displayed
             for player in top_players:  
                 print(f"\033[92m{player} \033[0m showed some incredible skill with amazing {most_wins} wins today!") #For looping top_players list and displaying highest scoring player(s)            
